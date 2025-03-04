a382ec2732d9d6091979db2a5e1bbd923960a9ac
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-03-04 11:59:37 +0530

    Create Intersection of 2 lists.py

---
+++ Python/Set Operations/Intersection of 2 lists.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def intersection(a, b):
+  # Create frequency count of elements in both lists
+  a = Counter(a)  # Dictionary-like object with element counts from lst1
+  b = Counter(b)  # Dictionary-like object with element counts from lst2
+
+  print(a&b)
+  
+  # Perform element-wise intersection (keeps min count from both lists) 
+  # ensuring duplicates are accounted for
+  return list(a & b)  # Returns a Counter with min occurrences in both lists
