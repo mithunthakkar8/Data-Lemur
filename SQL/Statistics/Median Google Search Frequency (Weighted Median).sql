80e86346c75fba52e7b218220cdfc6faa49ff7fb
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-03-10 11:00:11 +0530

    Create Median Google Search Frequency (Weighted Median).sql

---
+++ SQL/Statistics/Median Google Search Frequency (Weighted Median).sql
@@ -0,0 +1,11 @@
+with OrderedData as (
+    select searches
+        , sum(num_users) over () as total_users
+        , sum(num_users) over (order by searches) as cumulative_users
+    from search_frequency
+)
+select round(avg(searches),1) as median
+from OrderedData
+where cumulative_users between total_users/2
+    and total_users/2+1
+
