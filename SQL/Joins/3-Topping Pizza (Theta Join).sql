c2b8ea3d980f3080b8d6f75b5fcdf7a32b673099
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-03-11 12:36:57 +0530

    Update 3-Topping Pizza (Theta Join).sql

--- SQL/Joins/3-Topping Pizza (Theta Join).sql
+++ SQL/Joins/3-Topping Pizza (Theta Join).sql
@@ -1,7 +1,8 @@
-SELECT 
-    pt1.topping_name || ',' || pt2.topping_name || ',' || pt3.topping_name AS pizza,
-    pt1.ingredient_cost + pt2.ingredient_cost + pt3.ingredient_cost AS total_cost
-FROM pizza_toppings pt1
-JOIN pizza_toppings pt2 ON pt1.topping_name < pt2.topping_name
-JOIN pizza_toppings pt3 ON pt2.topping_name < pt3.topping_name
-ORDER BY total_cost DESC, pt1.topping_name, pt2.topping_name, pt3.topping_name;
+select 
+    pt1.topping_name || ',' || pt2.topping_name || ',' || pt3.topping_name as pizza
+    , pt1.ingredient_cost + pt2.ingredient_cost + pt3.ingredient_cost as total_cost
+from pizza_toppings pt1
+join pizza_toppings pt2 on pt1.topping_name < pt2.topping_name
+join pizza_toppings pt3 on pt2.topping_name < pt3.topping_name
+order by total_cost desc
+    , pt1.topping_name, pt2.topping_name, pt3.topping_name;
